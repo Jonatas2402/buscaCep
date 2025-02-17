50abb3d553c40fceeb3d1d3a4c09f7b2287d96f3
jonatas <jonatas_dev@hotmail.com>
2025-02-17 13:21:36 -0300

    Adicionando Comentários no projeto.

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,3 +1,9 @@
-public record Endereco(String cep, String logradouro, String bairro, String localidade, String uf, String estado, String regiao) {
-
+public record Endereco(String cep,
+                       String logradouro,
+                       String bairro,
+                       String localidade,
+                       String uf,
+                       String estado,
+                       String regiao) {
+    //A classe record só será responsável por salvar os dados que seram utilizados como parâmetro.
 }
